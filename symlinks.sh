8b74ca54cba903a3b027ed1673b3d27f132e137c
Carlos F. M. Faruolo <5carlosfelipe5@gmail.com>
2016-02-26 12:20:38 -0300

    Modificado o script para criar os links nas pastas hard-coded's.
    Movido layout.html para nova pasta html/

--- symlinks.sh
+++ symlinks.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
-# Criar os links simbolicos (linux/mac) para a pasta 'unirio'.
-# Uso: ./symlinks.sh pasta1 pasta2 ...
-# Sera feita uma copia do link simbolico em cada pasta passada como argumento.
+# Criar os links simbolicos (linux/mac) para os recursos da pasta 'unirio' (css, fonts, images, js e html).
+# Deve-se rodar o script nesta pasta (web2py_common/)!
+# O script cria pastas 'unirio' caso não existam dentro de static/css, static/fonts, static/images, static/js e views/
 
 # Pegando o diretorio atual expandido.
 path=$(cd .; pwd)
 
-# Para cada argumento passado, criar um link. Cada argumento sera tratado como um diretorio para se criar um link.
-for var in "$@"
-do
-    ln ${path}/unirio --target-directory="$var" --symbolic
-done
+# Se não existir pasta 'unirio' dentro de static/css, criar.
+
+# Remove se ja existem
+rm ../static/css/unirio
+rm ../static/fonts/unirio
+rm ../static/images/unirio
+rm ../static/js/unirio
+rm ../views/_unirio
+
+# Cria os links
+ln --symbolic ${path}/unirio/css ../static/css/unirio
+ln --symbolic ${path}/unirio/fonts ../static/fonts/unirio
+ln --symbolic ${path}/unirio/images ../static/images/unirio
+ln --symbolic ${path}/unirio/js ../static/js/unirio
+ln --symbolic ${path}/unirio/html ../views/_unirio
