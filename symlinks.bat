b6dd312258e2bd7f931b0657dd14dde9f215ea54
Carlos F. M. Faruolo <5carlosfelipe5@gmail.com>
2016-02-26 12:42:01 -0300

    Ajustado o script bat.

--- symlinks.bat
+++ symlinks.bat
@@ -1,16 +1,24 @@
 @echo off
-rem ToDo: Testar esse script
-rem Criar os links simbolicos (linux/mac) para a pasta 'unirio'.
-rem Uso: symlinks.bat pasta1 pasta2 ...
-rem Sera feita uma copia do link simbolico em cada pasta passada como argumento.
+rem Criar os links simbolicos (windows) para os recursos da pasta 'unirio' (css, fonts, images, js e html).
+rem Deve-se rodar o script nesta pasta (web2py_common/)!
+rem O script cria pastas 'unirio' caso não existam dentro de static/css, static/fonts, static/images, static/js e views/
+
+rem ToDo: Testar esse script!!!
 
 rem Pegando o diretorio atual expandido.
 set path=%~dp0
 
-rem Para cada argumento passado, criar um link. Cada argumento sera tratado como um diretorio para se criar um link.
-:loop_start
-if (%1) == () goto loop_end
-mklink "%1" %path%\unirio
-shift
-goto loop_start
-:loop_end
+rem Remove se ja existem
+del ../static/css/unirio
+del ../static/fonts/unirio
+del ../static/images/unirio
+del ../static/js/unirio
+del ../views/_unirio
+
+rem Cria os links
+
+mklink /D ..\static\css\unirio %path%\unirio\css
+mklink /D ..\static\fonts\unirio %path%\unirio\fonts
+mklink /D ..\static\images\unirio %path%\unirio\images
+mklink /D ..\static\js\unirio %path%\unirio\js
+mklink /D ..\views\_unirio %path%\unirio\html
